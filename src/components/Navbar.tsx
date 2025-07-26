a1b6e3c8ec575529cd811ece93662d688e75ce57
Vicky <vickyprasad2732003@gmail.com>
2025-07-26 13:08:37 +0530

    Refactor Navbar component to remove async and integrate SignInButton for authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+'use client';
 import { getAuthSession } from '@/lib/nextauth'
 import  Link  from 'next/link';
 import React from 'react'
+import SignInButton from './SignInButton';
 
 type Props = {}
 
-const Navbar = async (props: Props) => {
+const Navbar =  (props: Props) => {
     return(
         <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
             <div className='flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl'>
@@ -15,9 +17,12 @@ const Navbar = async (props: Props) => {
                 </p>
 
                 </Link>
+                <div className='flex items-center'>
+                    <SignInButton text="Sign In " />
+                </div>
             </div>
         </div>
-    )
+    );
  
 };
 
