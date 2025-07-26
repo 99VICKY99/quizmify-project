20925aceff0322dae48419154a4e6c0a6134fbb2
Vicky <vickyprasad2732003@gmail.com>
2025-07-26 14:13:52 +0530

    feat: add user account dropdown menu with sign out functionality
    - Added @radix-ui/react-dropdown-menu dependency for dropdown menu implementation.
    - Implemented UserAccountNav component to display user information and dropdown menu.
    - Created a new DropdownMenu component with various subcomponents for better reusability.
    - Integrated sign out functionality using next-auth in the dropdown menu.

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,5 +1,11 @@
+'use client';
 import React from 'react'
 import { User } from 'next-auth'
+import { DropdownMenu, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import { DropdownMenuContent } from './ui/dropdown-menu'
+import Link from 'next/link'
+import { signOut } from 'next-auth/react'
+import { Button } from './ui/button';
 
 type Props = {
     user: Pick<User,"name" | "email" | "image">
@@ -7,7 +13,40 @@ type Props = {
 
 const UserAccountNav = ({user}: Props) => {
   return (
-    <div>UserAccountNav</div>
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            {/*use avatar */}
+            <Button>hi</Button>
+            <DropdownMenuContent className='bg-white' align='end'>
+                <div className="flex items-center justify-start gap-2 p-2">
+                   <div className='flex flex-col space-y-1 leading-none'>
+                    {user.name && <p className='text-sm font-medium'>{user.name}</p>}
+                    {user.email && (
+                        <p className='w-[200px] truncate text-sm text-zinc-700'>
+                            {user.email}
+                        </p>
+                    )}
+                   </div>
+                </div>
+
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild>
+                    <Link href="/">Meow</Link>
+                </DropdownMenuItem> 
+
+                    <DropdownMenuSeparator />
+                <DropdownMenuItem  onClick={(e)=>{
+                    e.preventDefault();
+                    signOut().catch(console.error);
+                }}
+                className=' text-red-600 cursor-pointer'>
+                
+                    Sign Out
+                </DropdownMenuItem>
+
+                </DropdownMenuContent>
+        </DropdownMenuTrigger>
+    </DropdownMenu>
   )
 }
 
